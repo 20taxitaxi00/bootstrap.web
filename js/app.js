c6082311d70649f3dc81eb5cf63bd3c24b99b9fe
20taxitaxi00 <takushi0210@icloud.com>
2020-11-14 19:08:05 +0900

    グルナビAPIの仕組み完成しました！（レイアウトはてきとー）

--- js/app.js
+++ js/app.js
@@ -1,69 +1,80 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // document.getElementById('btn').addEventListener('click',function() {
+// ぐるなびAPI用
+// JavaScript 指定(授業コード)
+document.addEventListener('DOMContentLoaded',function(){
+  document.getElementById('submit').addEventListener('click', function(){
     ajaxRequest();
-  // });
+  });
 
-  function ajaxRequest() {
-    let result = document.getElementById('result');
-
+  // 関数ajaxRequest
+  function ajaxRequest(){
+    // 結果の取得
+    let result = document.getElementById('guru-result');
+    // 勉強する必要あり
     let xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
+    xhr.onreadystatechange = function(){
+      if(xhr.readyState === 4) {
         if(xhr.status === 200) {
           result.textContent = '';
-          // console.log(xhr.responseText);
           let res = JSON.parse(xhr.responseText);
-          console.log(res);
-          console.log(res.items);
-          for (item of res.items) {
+          console.log(res)
+          for(item of res.rest){
             renderResult(item);
           }
-
         } else {
           result.textContent = 'サーバーエラー';
         }
       } else {
         result.textContent = '通信中';
       }
-    }
-    
-    let url = 'https://www.googleapis.com/youtube/v3/search?key=';
-    let key = 'AIzaSyBwD5ERbKOao5EX4t30cp846VgAbQ959Js';
-    let part = '&part=snippet&maxResults=1';
-    let q = '&q=サッカー'
-    let type = '&type=video';
-    xhr.open('GET', url + key + part + q + type, true);
+    };
+
+    // グルナビAPIと自分のキー
+    let url = 'https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=8b9899ee8b786d216eb0a28404ba7c4b';
+    // 取得するデータ数
+    // let part = '&part=snippet&maxResults=9';
+    let area = '&area=AREA200';
+    let areacode_s = '&areacode_s=AREAS8252';
+    // let freeword_value = document.getElementById('key').value;
+    let freeword = '&freeword=' + document.getElementById('key').value;
+    // 指定変数の組み合わせ
+    xhr.open('GET', url + area + areacode_s + freeword, true);
     xhr.send(null);
-  }
+  };
 
-  function renderResult(item) {
-    
-    //取得した内容を変数に格納
-    let title = item.snippet.title;
-    let thumbnail = item.snippet.thumbnails.medium;
-    let videoId = item.id.videoId;
 
-    //要素を組み立てる
+  function renderResult(item){
+
+    // 取得した内容を変数に格納
+    let title = item.name;
+    let open = item.opentime;
+    let image = item.image_url;
+    let link = item.url;
+
+    // 要素の組み立て
     let img = document.createElement('img');
     let anchor = document.createElement('a');
     let text = document.createElement('p');
-    let li = document.createElement('li');
+    let opentime = document.createElement('p');
+    let div = document.createElement('div');
+    div.classList.add('col-6');
 
-    img.src = thumbnail.url;
-    img.alt = title;
-    img.width = thumbnail.width;
-    img.height = thumbnail.height;
-    
-    anchor.href = 'https://www.youtube.com/watch?v=' + videoId;
+    img.src = image.shop_image1;
+    // img.width = image.width;
+    // img.height = image.height;
+
+    anchor.href = link;
 
     text.textContent = title;
-
-    //画面に表示
+    opentime.textContent = open;
+    // 画面に表示
     anchor.appendChild(img);
-    li.appendChild(anchor);
-    li.appendChild(text);
-    document.getElementById('result').appendChild(li);
-  }
+    div.appendChild(anchor);
+    div.appendChild(text);
+    div.appendChild(opentime);
+    document.getElementById('guru-result').appendChild(div);
+
+  };
+
+
 
 });
